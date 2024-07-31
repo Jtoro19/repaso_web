c6070d006daafd769f641fe67b470ed9a1f4ce0c
Jtoro19 <jutorop@unal.edu.co>
2024-07-31 11:25:26 -0500

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BooksController;
+
+
+Route::post('/books', [BooksController::class, 'store'])->name('books.store');
+Route::get('/', function () {
+    return view('books');
+});
